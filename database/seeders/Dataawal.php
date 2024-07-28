e6bd5c8976ac7fba5fb89e0a359149c25523305d
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-07-28 11:24:40 +0700

    setting auth admin, with dummy data

---
+++ database/seeders/Dataawal.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class Dataawal extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = new User();
+        $user->name = "Admin";
+        $user->email = "admin@gmail.com";
+        $user->password = bcrypt("12345678");
+        $user->peran ="admin";
+        $user->save();
+    }
+}
