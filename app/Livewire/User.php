ba86af55eaa91cd0bd6395dface591831d1dadd6
Muhammad Susilo <muhammadsusilo43@gmail.com>
2024-07-28 14:03:14 +0700

    create content user

--- app/Livewire/User.php
+++ app/Livewire/User.php
@@ -3,11 +3,21 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\User as ModelUser;
 
 class User extends Component
 {
+    public $pilihanMenu = "lihat";
+
+    public function pilihMenu($menu)
+    {
+        $this->pilihanMenu = $menu;
+    }
+
     public function render()
     {
-        return view('livewire.user');
+        return view('livewire.user')->with([
+            "semuaPengguna" => ModelUser::all()
+        ]);
     }
 }
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -1,3 +1,98 @@
 <div>
-    {{-- Do your work, then step back. --}}
+    <div class="container">
+        <div class="row my-2">
+            <div class="col-12">
+                <button wire:click="pilihMenu('lihat')"
+                    class="btn {{ $pilihanMenu == 'lihat' ? 'btn-success' : 'btn-outline-success' }}">
+                    Semua Pengguna
+                </button>
+                <button wire:click="pilihMenu('tambah')"
+                    class="btn {{ $pilihanMenu == 'tambah' ? 'btn-success' : 'btn-outline-success' }}">
+                    Tambah Pengguna
+                </button>
+                <button wire:loading class="btn btn-info">
+                    Loading ...
+                </button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                @if ($pilihanMenu == 'lihat')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Semua Pengguna
+                        </div>
+                        <div class="card-body">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <th>No</th>
+                                    <th>Nama</th>
+                                    <th>Email</th>
+                                    <th>Peran</th>
+                                    <th>Data</th>
+                                </thead>
+                                <tbody>
+                                    @foreach ($semuaPengguna as $pengguna)
+                                        <tr>
+                                            <td>{{ $pengguna['id'] }}</td>
+                                            <td>{{ $pengguna['name'] }}</td>
+                                            <td>{{ $pengguna['email'] }}</td>
+                                            <td>{{ $pengguna['peran'] }}</td>
+                                            <td>
+                                                <button wire:click="pilihMenu('edit')" class="btn btn-sm btn-warning">
+                                                    Edit
+                                                </button>
+                                                <button wire:click="pilihMenu('hapus')"
+                                                    class="btn btn-sm btn-danger">
+                                                    Hapus
+                                                </button>
+                                                {{-- <button wire:click="pilihMenu('edit')"
+                                                    class="btn {{ $pilihanMenu == 'edit' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    Edit Pengguna
+                                                </button> --}}
+                                                {{-- <button wire:click="pilihMenu('hapus')"
+                                                    class="btn {{ $pilihanMenu == 'hapus' ? 'btn-primary' : 'btn-outline-primary' }}">
+                                                    Edit Pengguna
+                                                </button> --}}
+                                            </td>
+                                        </tr>
+                                        </tr>
+                                    @endforeach
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @elseif($pilihanMenu == 'tambah')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Tambah Pengguna
+                        </div>
+                        <div class="card-body">
+                            tes
+                        </div>
+                    </div>
+                @elseif($pilihanMenu == 'edit')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Edit Pengguna
+                        </div>
+                        <div class="card-body">
+                            tes
+                        </div>
+                    </div>
+                @elseif($pilihanMenu == 'hapus')
+                    <div class="card border-primary">
+                        <div class="card-header">
+                            Hapus Pengguna
+                        </div>
+                        <div class="card-body">
+                            tes
+                        </div>
+                    </div>
+                @endif
+
+            </div>
+        </div>
+    </div>
 </div>
